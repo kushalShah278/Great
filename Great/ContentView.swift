4cc7d829e920d2a937418f9eec72d9391db18257
kushal <shahkushal278@gmail.com>
2024-01-20 19:50:36 -0500

    Initial Commit

---
+++ Great/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Great
+//
+//  Created by Kushal shah on 2024-01-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Great/GreatApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreatApp.swift
+//  Great
+//
+//  Created by Kushal shah on 2024-01-20.
+//
+
+import SwiftUI
+
+@main
+struct GreatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
